25ef10a2733c0da7f6741566d2fb26b303b5e08a
Andre Honorio <andresilva@setic.ro.gov.br>
2021-10-26 01:29:07 -0400

    deleção da pasta folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'inicio',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'lanches',
+    loadChildren: () => import('./cardapio/lanches/lanches.module').then( m => m.LanchesPageModule)
+  },
+  {
+    path: 'pratos',
+    loadChildren: () => import('./cardapio/pratos/pratos.module').then( m => m.PratosPageModule)
+  },
+  {
+    path: 'sobremesas',
+    loadChildren: () => import('./cardapio/sobremesas/sobremesas.module').then( m => m.SobremesasPageModule)
+  },
+  {
+    path: 'bebidas',
+    loadChildren: () => import('./cardapio/bebidas/bebidas.module').then( m => m.BebidasPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   }
 ];
 
